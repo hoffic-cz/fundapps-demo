1c5ab2e1a001144ce3c6d76345ffc293dd63e0df
Petr Hoffmann <petr.hoffic@gmail.com>
2019-11-08 12:01:45 +0000

    Implemented radio communication.

--- src/main/java/cz/hoffic/fundappsdemo/Radio.java
+++ src/main/java/cz/hoffic/fundappsdemo/Radio.java
@@ -2,11 +2,24 @@ package cz.hoffic.fundappsdemo;
 
 public class Radio {
 
-  public void send(String message) {
+  private StringBuilder output;
+  private boolean used = false;
 
+  public Radio() {
+    output = new StringBuilder();
+  }
+
+  public void send(String message) {
+    if (used) {
+      output.append("\n");
+    } else {
+      used = true;
+    }
+
+    output.append(message);
   }
 
   public String receive() {
-    return null;
+    return output.toString();
   }
 }
