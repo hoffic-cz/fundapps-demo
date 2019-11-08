8e058e7aca7e30e2c4a47d7404376bac7448caa3
Petr Hoffmann <petr.hoffic@gmail.com>
2019-11-08 11:00:50 +0000

    Added tests for forward and backward movement.

---
+++ src/main/java/cz/hoffic/fundappsdemo/Navigation.java
@@ -0,0 +1,24 @@
+package cz.hoffic.fundappsdemo;
+
+public class Navigation {
+
+  private int x = 0;
+  private int y = 0;
+  private char heading = 'N';
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public char getHeading() {
+    return heading;
+  }
+
+  public void command(String commands) {
+    return;
+  }
+}
---
+++ src/test/java/cz/hoffic/fundappsdemo/NavigationTest.java
@@ -0,0 +1,69 @@
+package cz.hoffic.fundappsdemo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class NavigationTest {
+
+  @Test
+  public void initialState() {
+    var navigation = new Navigation();
+
+    assertEquals(0, navigation.getX());
+    assertEquals(0, navigation.getY());
+    assertEquals('N', navigation.getHeading());
+  }
+
+  @Test
+  public void forwardAndBackwards() {
+    var navigation = new Navigation();
+
+    navigation.command("F");
+
+    assertEquals(0, navigation.getX());
+    assertEquals(1, navigation.getY());
+    assertEquals('N', navigation.getHeading());
+
+    navigation.command("B");
+
+    assertEquals(0, navigation.getX());
+    assertEquals(0, navigation.getY());
+    assertEquals('N', navigation.getHeading());
+  }
+
+  @Test
+  public void multipleForwardAndBackwards() {
+    var navigation = new Navigation();
+
+    navigation.command("FF");
+
+    assertEquals(0, navigation.getX());
+    assertEquals(2, navigation.getY());
+    assertEquals('N', navigation.getHeading());
+
+    navigation.command("BB");
+
+    assertEquals(0, navigation.getX());
+    assertEquals(0, navigation.getY());
+    assertEquals('N', navigation.getHeading());
+  }
+
+  @Test
+  public void backFirstThenForward() {
+    var navigation = new Navigation();
+
+    navigation.command("B");
+
+    assertEquals(0, navigation.getX());
+    assertEquals(-1, navigation.getY());
+    assertEquals('N', navigation.getHeading());
+
+    navigation.command("F");
+
+    assertEquals(0, navigation.getX());
+    assertEquals(0, navigation.getY());
+    assertEquals('N', navigation.getHeading());
+  }
+}
