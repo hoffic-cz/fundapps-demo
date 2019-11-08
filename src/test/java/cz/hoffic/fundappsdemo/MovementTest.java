179752cb12e8b3a9d0b9555d251ebeca42aaf1ef
Petr Hoffmann <petr.hoffic@gmail.com>
2019-11-08 11:11:34 +0000

    Added tests for turning.

--- src/test/java/cz/hoffic/fundappsdemo/MovementTest.java
+++ src/test/java/cz/hoffic/fundappsdemo/MovementTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class NavigationTest {
+public class MovementTest {
 
   @Test
   public void initialState() {
---
+++ src/test/java/cz/hoffic/fundappsdemo/TurningTest.java
@@ -0,0 +1,86 @@
+package cz.hoffic.fundappsdemo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TurningTest {
+
+  @Test
+  public void turnRight() {
+    var navigation = new Navigation();
+
+    assertEquals('N', navigation.getHeading());
+
+    navigation.command("R");
+
+    assertEquals('E', navigation.getHeading());
+  }
+
+  @Test
+  public void turnLeft() {
+    var navigation = new Navigation();
+
+    assertEquals('N', navigation.getHeading());
+
+    navigation.command("L");
+
+    assertEquals('W', navigation.getHeading());
+  }
+
+  @Test
+  public void turningBackAndForth() {
+    var navigation = new Navigation();
+
+    assertEquals('N', navigation.getHeading());
+
+    navigation.command("LR");
+
+    assertEquals('N', navigation.getHeading());
+
+    navigation.command("RL");
+
+    assertEquals('N', navigation.getHeading());
+  }
+
+  @Test
+  public void turningTwice() {
+    var navigation = new Navigation();
+
+    assertEquals('N', navigation.getHeading());
+
+    navigation.command("LL");
+
+    assertEquals('S', navigation.getHeading());
+
+    navigation.command("RR");
+
+    assertEquals('N', navigation.getHeading());
+  }
+
+  @Test
+  public void turningAround() {
+    var navigation = new Navigation();
+
+    assertEquals('N', navigation.getHeading());
+
+    navigation.command("RRRR");
+
+    assertEquals('N', navigation.getHeading());
+
+    navigation.command("LLLL");
+
+    assertEquals('N', navigation.getHeading());
+  }
+
+  @Test
+  public void movementHasNoEffect() {
+    var navigation = new Navigation();
+
+    assertEquals('N', navigation.getHeading());
+
+    navigation.command("FFBF");
+
+    assertEquals('N', navigation.getHeading());
+  }
+}
